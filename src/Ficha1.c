9d6725afaff5170101e27c088cabb9216a2b90f9
jcqornelas <114989257+jcqornelas@users.noreply.github.com>
2022-10-28 17:38:28 +0100

    OLA

--- src/Ficha1.c
+++ src/Ficha1.c
@@ -12,6 +12,78 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+//================Alinea 1 ==============================//
+	float pi = 3.14;
+	float raio = 2.5;
+
+	float area = pi*raio*raio;
+
+
+//========================Alinea 2 =========================//
+    int lado=1;
+    int altura=3;
+	int perimetro= 2* (lado+altura);
+	printf("%i",perimetro);
+
+
+//=========================Alinea 3 =======================//
+
+int a = 5;
+int b = 3;
+int c = 7;
+int d= 10;
+int e = 1;
+
+float media =(a+b+c+d+e)/ 5.0;
+printf("%f", media);
+
+
+
+
+
+//=====================Alinea 4 =========================//
+
+
+ float fahr = 212;
+ float celsius = (fahr - 32) / 1.8;
+ printf()
+
+
+
+
+
+
+//================== Alinea 5===========================//
+
+ float distancia = 370;
+ float consumo = 40;
+ float kms= 100 ;
+ float media = (100* 40) / 370;
+printf(" A media do seu auto aos %.1f é %.1f, kms , media");
+
+
+
+//================= Alinea 6========================//
+
+
+int horas = 8;
+int min = 22;
+int segundos= 10;
+int total= horas*3600+22*60+10;
+printf("total%d",total);
+
+
+
+
+
+
+
+
+
+
+
+
+return EXIT_SUCCESS;
 }
+
